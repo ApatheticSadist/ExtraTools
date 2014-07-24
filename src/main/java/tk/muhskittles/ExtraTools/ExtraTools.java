e1948f8d047a5a110991a39d4110d4f2f4ead08f
ApatheticSadist <trolldieoctoganon@gmail.com>
2014-07-24 14:37:12 -0500

    b

---
+++ src/main/java/tk/muhskittles/ExtraTools/ExtraTools.java
@@ -0,0 +1,17 @@
+package tk.muhskittles.ExtraTools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExtraTools extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
